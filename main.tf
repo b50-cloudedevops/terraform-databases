3ed967e65da25a3ea30087ae861e57011c9e6177
vamsi434 <vamsibusa@gmail.com>
2022-11-22 19:24:40 +0530

    tsg

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
 module "mongodb" {
     source = "./vendor/modules/mongodb"
     ENV = var.ENV 
+    VPC_CIDR = var.VPC_CIDR
+    PUBLIC_SUBNET_CIDR = var.PUBLIC_SUBNET_CIDR
+    PRIVATE_SUBNET_CIDR = var.PRIVATE_SUBNET_CIDR
+    AZ = var.AZ
+    DEFAULT_VPC_ID = var.DEFAULT_VPC_ID
+    DEFAULT_VPC_CIDR = var.DEFAULT_VPC_CIDR
+    DEFAULT_VPC_RT = var.DEFAULT_VPC_RT
 }
 //variable "PUBLIC_SUBNET_CIDR" {}
 //variable "PRIVATE_SUBNET_CIDR" {}
